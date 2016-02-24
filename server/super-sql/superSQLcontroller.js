c8d9ce5514bd02794e6105951a008f55d8ab192a
Rob Rosario <robbysrosario@gmail.com>
2016-02-23 16:16:37 -0800

    added the beginning of the nosql method

--- server/super-sql/superSQLcontroller.js
+++ server/super-sql/superSQLcontroller.js
@@ -27,16 +27,20 @@ superSQLController.createSQLModel = createSQLModel;
     };
 
     function createSQLModel(dataChunk) {
+
         // const db = req.body.schema;  //production variable
         const db    = dataChunk.schema;
         const dbData = dataChunk.schema.data.colORdocs;
 
-        //templating for the top require sequelize and Constructor
+        //Require SQL
         const _RequireSQL  = `var Sequelize = require('sequelize')`;
+        //Create instance of Sequelize
         const _SQLdataBase = `var sequelize = new Sequelize ( ${db.database.url}, ${db.database.username}, ${db.database.password})`;
 
-        //SQL MODEL
+        //SQL MODEL Object
         const _instanceData =  {};
+
+        //create Model Obj key: values
         dbData.forEach( (value) => {
                                       for(var key in dbData){
                                           for (var item in value) {
@@ -47,15 +51,52 @@ superSQLController.createSQLModel = createSQLModel;
                                           }
                                    });
 
+        //Create Model
         let _SQLinstance  = `var ${db.database.table} = sequelize.define(${db.database.table}, ${_instanceData})`;
-        console.log('_instanceDATA: ', _instanceData);
-        console.log(db);
+    }
+
+
+    function createNoSQLModel(dataChunk) {
+
+        // const db = req.body.schema;  //production variable
+        const db    = dataChunk.schema;
+        const dbData = dataChunk.schema.data.colORdocs;
+
+        //Require SQL
+        const _RequireMongoose  = `var mongoose = require('mongoose');`;
+        //Create instance of Sequelize
+        const _Mongooseconnect = `mongoose.connect( mongodb://${db.database.url} );`;
+
+        //Mongoose MODEL Object
+        const _instanceData =  {};
+
+        //create Model Obj key: values
+        dbData.forEach( (value) => {
+                                      for(var key in dbData){
+                                          for (var item in value) {
+                                              console.log('key:', item);
+                                              console.log('value:', value[item]);
+                                              _instanceData[item] = value[item];
+                                            }
+                                          }
+                                   });
+
+        //Create Model
+        // CREATE MONGOOSE MODEL////////////////////////////////////
+        // NOTE : This is the mongoose model
+        // Example:  var Cat = mongoose.model('Cat',{name: String};
+        // /////////////////////////////////////////////
+        let _Mongooseinstance  = `var ${dataChunk.data.table} = mongoose.model(${db.database.table}, ${_instanceData})`;
+
+        console.log(_RequireMongoose);
+        console.log(_Mongooseconnect);
+        console.log(_Mongooseinstance);
 
     }
 
  // console.log(JSON.stringify(dataChunk, null, '\t'));
 
- createSQLModel(dataChunk);
+ createNoSQLModel(dataChunk);
 
 
 module.exports = superSQLController;
